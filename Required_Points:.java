099bbc4f90a14ad548558ca210bc5cef7717f07c
BollaRimcy <157674805+BollaRimcy@users.noreply.github.com>
2024-01-29 23:38:44 +0530

    import java.util.Scanner; public class Max{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         double c=b/a;         System.out.printf("%.0f",c);     } }

---
+++ Required_Points:.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Max{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        double c=b/a;
+        System.out.printf("%.0f",c);
+    }
+}
