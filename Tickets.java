1fa045cef11897321a64c21bb579a8fa0538676f
BollaRimcy <157674805+BollaRimcy@users.noreply.github.com>
2024-02-01 19:55:24 +0530

    import java.util.Scanner; public class Ticket{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         if(4*x>1000){             System.out.println("NO");         }         else{             System.out.println("YES");         }     } }

---
+++ Tickets.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Ticket{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        if(4*x>1000){
+            System.out.println("NO");
+        }
+        else{
+            System.out.println("YES");
+        }
+    }
+}
