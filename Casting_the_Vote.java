cc58114118a228d8a0a56140ef0bf3fe98322e93
BollaRimcy <157674805+BollaRimcy@users.noreply.github.com>
2024-02-01 21:16:27 +0530

    import java.util.*; public class Vote{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         if(x>=18){             System.out.print("YES");         }         else{             System.out.print("NO");         }     } }

---
+++ Casting_the_Vote.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Vote{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x>=18){
+            System.out.print("YES");
+        }
+        else{
+            System.out.print("NO");
+        }
+    }
+}
