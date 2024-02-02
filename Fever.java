458ad47bf00c9fa40efe5a23e33a2fff087f5d2a
BollaRimcy <157674805+BollaRimcy@users.noreply.github.com>
2024-02-02 08:26:17 +0530

    import java.util.*; public class Fever{ public static void main(String args[]){     Scanner sc=new Scanner(System.in);     int a=sc.nextInt();     if(a<=98){         System.out.print("NO");     }     else{         System.out.print("YES");     } } }

---
+++ Fever.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Fever{
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    if(a<=98){
+        System.out.print("NO");
+    }
+    else{
+        System.out.print("YES");
+    }
+}
+}
