35394bca3ebdb7e44839e48b59d7eb388cf3b37a
BollaRimcy <157674805+BollaRimcy@users.noreply.github.com>
2024-02-02 09:41:14 +0530

    import java.util.*; public class Reach{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         if((5*x)>=y){             System.out.print("YES");         }         else{             System.out.print("NO");         }     } }

---
+++ Reach_Home.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Reach{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if((5*x)>=y){
+            System.out.print("YES");
+        }
+        else{
+            System.out.print("NO");
+        }
+    }
+}
