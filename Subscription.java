7c022c578eca8316e537e794166100692f774e99
BollaRimcy <157674805+BollaRimcy@users.noreply.github.com>
2024-02-02 19:23:02 +0530

    import java.util.*;  public class Sub{      public static void main(String args[]){          Scanner sc=new Scanner(System.in);          int s=sc.nextInt();          if(s>40)          {              System.out.print("YES");          }          else          {              System.out.print("NO");          }      }  }

---
+++ Subscription.java
@@ -0,0 +1,15 @@
+import java.util.*;
+ public class Sub{
+     public static void main(String args[]){
+         Scanner sc=new Scanner(System.in);
+         int s=sc.nextInt();
+         if(s>40)
+         {
+             System.out.print("YES");
+         }
+         else
+         {
+             System.out.print("NO");
+         }
+     }
+ }
