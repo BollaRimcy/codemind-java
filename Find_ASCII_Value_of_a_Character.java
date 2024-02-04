ddd100dc233382da4b07f22ffbd4e261b37f0d9d
BollaRimcy <157674805+BollaRimcy@users.noreply.github.com>
2024-02-04 19:34:40 +0530

    import java.util.*; public class Asc{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         char ch=sc.next().charAt(0);         int asc=(int)ch;         System.out.print(asc);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Asc{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        int asc=(int)ch;
+        System.out.print(asc);
+    }
+}
